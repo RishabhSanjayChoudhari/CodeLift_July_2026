eb2211f0f967c4f372bc23b80876f4f36bcf8ce5
Rishabh Choudhari <123304536+rishbh2411@users.noreply.github.com>
2026-07-20 21:32:29 +0530

    printOrderReceipt function added

--- IMS.py
+++ IMS.py
@@ -4,4 +4,11 @@ class App:
         print('Enter ItemId : ')
         for k,v in Data.items.items():
             print(k,v["name"],v["price"])
-    printMenu()
+    def printOrderReceipt(orderId):
+        order = Data.orders[orderId]
+        itemList = order["cart"]
+        for item in itemList:
+            productName = Data.items[item["product_id"]]["name"]
+            itemTotal = item["price"]*item["quantity"]
+            print(productName,"$",item["price"],'x',item["quantity"],'=','$',itemTotal)
+    printOrderReceipt(102)
