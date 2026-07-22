a2c1470c7e890bdfd565eb8588b4b3991f2d393c
Prince Tiwari <princetiwari9511@gmail.com>
2026-07-22 21:30:47 +0530

    Print Receipt Update
    Print Receipt Update

--- Data.py
+++ Data.py
@@ -9,22 +9,22 @@ class Data:
     items = {
         1: {
             "name": "milk",
-            "price": 100,
-            "quantity": 50,
+            "price": 50,
+            "quantity": 500,
             "unit": Unit.LTR,
             "is_active": True,
         },
         2: {
             "name": "sugar",
-            "price": 50,
+            "price": 100,
             "quantity": 100,
             "unit": Unit.KG,
             "is_active": True,
         },
         3: {
             "name": "Parle-G",
-            "price": 5,
-            "quantity": 1000,
+            "price": 10,
+            "quantity": 100,
             "unit": Unit.PIECES,
             "is_active": True,
         },
@@ -41,32 +41,34 @@ class Data:
         "SUPER10": {
             "discount_type": "percent_off",
             "minimum_purchase": 500,
-            "maximum_purchase": 1000,
-            "discount_value": 20,
+            "maximum_purchase": 100000,
+            "discount_value": 200,
             "is_active": True,
         },
         "10SUPER": {
             "discount_type": "amount_off",
             "minimum_purchase": 1000,
-            "maximum_purchase": 2000,
-            "discount_value": 100,
+            "maximum_purchase": 20000000,
+            "discount_value": 500,
             "is_active": True,
         },
     }
 
     orders = {
-        101: {
+        "101": {
             "cart": [
-                {"product_id": 1, "quantity": 20, "price": 200},
+                {"product_id": 1, "quantity": 5, "price": 50},
                 {"product_id": 2, "quantity": 10, "price": 100},
-                {"product_id": 3, "quantity": 200, "price": 1000},
-            ]
+                {"product_id": 3, "quantity": 200, "price": 10},
+            ],
+            "promotions": "10SUPER"
         },
-        102: {
+        "102": {
             "cart": [
-                {"product_id": 1, "quantity": 20, "price": 200},
+                {"product_id": 1, "quantity": 20, "price": 50},
                 {"product_id": 2, "quantity": 10, "price": 100},
-                {"product_id": 3, "quantity": 200, "price": 1000},
-            ]
+                {"product_id": 4, "quantity": 150, "price": 10},
+            ],
+            "promotions": "SUPER10"
         }
     }
--- IMS.py
+++ IMS.py
@@ -4,4 +4,40 @@ class App:
         print('Enter ItemId : ')
         for k,v in Data.items.items():
             print(k,v["name"],v["price"])
+    
+    
+    def printOrder():
+        orderId = input('Enter your orderId: ')
+        print("")
+        print("=" * 50)
+        print('OrderId: ' , orderId)
+        print("-" * 50)
+        fulltotal = 0
+        for item in Data.orders[orderId]["cart"]:
+            qnt = item['quantity']
+            amount =  item['price']
+            total = item['quantity']  * item['price']
+            print(amount,'x', qnt,'=', total)
+            fulltotal +=total
+        print("-" * 50)
+        print("Subtotal : ₹", fulltotal)
+
+        code = Data.orders[orderId]["promotions"]
+
+        if code in Data.promotions:
+            discount = Data.promotions[code]["discount_value"]
+
+            fulltotal = fulltotal - discount
+
+            print("Coupon Applied :", code)
+            print("Discount       : ₹", discount)
+            print("-" * 50)
+            print("Total After Discount : ₹", fulltotal)
+            print("+GST                 : 18% ")
+            print('Total After Taxes    :', (fulltotal*0.18)+fulltotal)
+            
+
+        print("=" * 50)
+
     printMenu()
+    printOrder()
